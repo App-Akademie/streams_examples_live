29830565427aa852e13418401c69bab5882e8f4d
David Sent <david.sent@app-akademie.com>
2025-06-30 12:04:58 +0200

    Remove commented-out code for location printing in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,6 @@ import 'package:streams_examples_live/location/location.dart';
 void main() async {
   final locations = generateLocations();
 
-  // await for (Location location in locations) {
-  //   print(location);
-  // }
-
   locations.listen(
     (l) {
       print("Neue Location \\o/ lat: ${l.latitude}, long: ${l.longitude}");
